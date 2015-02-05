d633cbdfdfb32e87551145566255d32f10bfeb9a
saforer <saforer@gmail.com>
2015-02-05 12:45:38 -0500

    Kindaworking

--- core/src/com/lock/entities/DriverPin.java
+++ core/src/com/lock/entities/DriverPin.java
@@ -1,8 +1,8 @@
 package com.lock.entities;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
 
 
@@ -13,17 +13,25 @@ public class DriverPin {
 	public DriverPin(Vector2 pos) {
 		this.pos = pos;
 		height = 20;
+
+		setShape();
+	}
+	
+	void setShape() {
 		int width = 10;
-		shape = new float[] {0 , 0, width, 20, width, 20 + height, -width, 20 + height, -width, 20};
+		
+		shape = new float[] {width, 0, width, height, -width, height, -width, 0};
 	}
 	
 	public int getHeight() { return height; }
 	
 	public void draw(ShapeRenderer sr) {
-		sr.translate(pos.x, pos.y, 0);
-		sr.setColor(Color.WHITE);
-		sr.begin(ShapeType.Line);		
-		sr.polygon(shape);
+		Polygon dripoly = new Polygon(shape);
+		
+		dripoly.setPosition(pos.x,pos.y);
+		
+		sr.begin(ShapeType.Line);
+		sr.polygon(dripoly.getTransformedVertices());
 		sr.end();
 	}
 }
--- core/src/com/lock/entities/KeyPin.java
+++ core/src/com/lock/entities/KeyPin.java
@@ -1,10 +1,9 @@
 package com.lock.entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
 
 public class KeyPin {
@@ -26,10 +25,16 @@ public class KeyPin {
 	void setShape() {
 		int width = 10;
 		
-		
+		shape = new float[] {0 , 0, width, 20, width, 20 + height, -width, 20 + height, -width, 20};
 	}
 	
 	public void draw(ShapeRenderer sr) {
+		Polygon keypoly = new Polygon(shape);
 		
+		keypoly.setPosition(pos.x,pos.y);
+		
+		sr.begin(ShapeType.Line);
+		sr.polygon(keypoly.getTransformedVertices());
+		sr.end();
 	}
 }
